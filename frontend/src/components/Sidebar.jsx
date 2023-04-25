62d0aebeb2985ef613edd1ecde21a7b807ee3513
Gracelyn Prajna <gracelynprajna@gmail.com>
2023-04-25 18:58:53 -0400

    made a navbar, components folder, css folder

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import '/css/Sidebar.css'
+
+const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+      <button className="sidebar-toggle" onClick={toggleSidebar}>
+      </button>
+      <nav className="sidebar-nav">
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Sidebar;
