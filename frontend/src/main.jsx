c40c02ae8d11f27808f2307682b882ea998406f5
imkewlhuh <rafael.lopez0827@gmail.com>
2023-04-24 23:13:46 -0400

    setup react router, removed App.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+//Add your imports from "/pages" below this line
+import Home from './pages/home';
+import SignUp from './pages/signUp';
+
+//Creating Router instance
+const router = createBrowserRouter([
+  {
+    //Landing page/Home page
+    path: "/",
+    element: <Home />,
+    children: [
+      //Rest of pages belong here, when you finish making a page
+      //add a new object with url in path and imported page in element
+      {
+        path: "/login",
+        element: <div>This is the Login Page</div>
+      },
+      {
+        path: "/signup",
+        element: <SignUp />
+      },
+      {
+        path: "/dashboard",
+        element: <div>Company Dashboard</div>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
