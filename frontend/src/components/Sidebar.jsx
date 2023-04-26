ad96539da7be42b675201bdafa8ed7e97ff05120
imkewlhuh <rafael.lopez0827@gmail.com>
2023-04-26 00:58:57 -0400

    sidebar color + content added, hover to expand

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,31 +1,26 @@
-import React, { useState } from 'react';
 import '/css/Sidebar.css'
 
 const Sidebar = () => {
-  const [isOpen, setIsOpen] = useState(true);
-
-  const toggleSidebar = () => {
-    setIsOpen(!isOpen);
-  };
 
   return (
-    <div className={`sidebar ${isOpen ? 'open' : ''}`}>
-      <button className="sidebar-toggle" onClick={toggleSidebar}>
-      </button>
-      <nav className="sidebar-nav">
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+   <div className="sidebar">
+    <nav>
+      <ul className="directories">
+        <li><a href='#'><i class="bi bi-pip"></i><span>Dashboard</span></a></li>
+        <li><a href='#'><i class="bi bi-clipboard-check"></i><span>Inventory</span></a></li>
+        <li><a href='#'><i class="bi bi-shield-fill-check"></i><span>Orders</span></a></li>
+        <li><a href='#'><i class="bi bi-building"></i><span>Suppliers</span></a></li>
+        <li><a href='#'><i class="bi bi-globe"></i><span>Sustainability</span></a></li>
+        <li><a href='#'><i class="bi bi-columns-gap"></i><span>Integrations</span></a></li>
+        <li><a href='#'><i class="bi bi-file-bar-graph"></i><span>Analytics</span></a></li>
+        <li><a href='#'><i class="bi bi-people"></i><span>Teams</span></a></li>
+      </ul>
+      <ul className="settings">
+      <li><a href='#'><i class="bi bi-gear"></i><span>Settings</span></a></li>
+      <li><a href='#'><i class="bi bi-question-circle"></i><span>Help Center</span></a></li>
+      </ul>
+    </nav>
+   </div>
   );
 };
 
