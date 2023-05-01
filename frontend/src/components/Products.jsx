e6e201f8317650ca91bb8a3426fee1bd37a1d8f2
Qojedi57 <111308861+Qojedi57@users.noreply.github.com>
2023-04-30 22:53:02 -0700

    Hotfix:deleted the commented code cleaned up the file

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -4,39 +4,7 @@ import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 
-
-// const Products = ({productItems}) => {
-//   return (
-//     <div className="products">
-//         {productItems.map((productItem) =>(
-//             <div key={productItem.id} className="card">
-//                 <div>
-//                     <img
-//                     className="product-image"
-//                     src={productItem.image}
-//                      alt={productItem.name}
-//                      />
-//                 </div>
-
-//                 <div>
-//                     <h3 className="product-name">{productItem.name}</h3>
-//                 </div>
-//                 <div className="product-price">{productItem.price}</div>
-
-//                 <div>
-//                     <h3 className="product-units">{productItem.units}</h3>
-//                 </div>
-//                 </div>
-//         ))}
-//        </div>
-//   );
-// };
-
-// export default Products;
-
 const Products = ({ productItems }) => {
-    const totalPrice = productItems.reduce((total, product) => total + product.price, 0);
-  const totalUnits = productItems.reduce((total, product) => total + product.units, 0);
   return (
     <Row xs={1} md={2} className="g-4">
       {productItems.map((productItem) => (
