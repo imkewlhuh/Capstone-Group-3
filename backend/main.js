626f0576aedf7b8c58bc7f0992701fb9aa7534e9
imkewlhuh <rafael.lopez0827@gmail.com>
2023-06-14 19:54:10 -0400

    Fixed esbuild/darwin-x64 issue, prep 4 production

--- backend/main.js
+++ backend/main.js
@@ -1,7 +1,10 @@
 import createServer from "./server.js";
+import { baseURL } from "../frontend/src/api/constants.js"
 
 const server = createServer();
 
-server.listen(8080, () => {
-    console.log("App is listening at http://localhost:8080");
+const port = process.env.PORT || 8080;
+
+server.listen(port, () => {
+    console.log(`App is listening at ${baseURL}`);
 });
--- frontend/src/api/constants.js
+++ frontend/src/api/constants.js
@@ -4,8 +4,8 @@ if (process.env.NODE_ENV === 'development') {
     //If you have a different local host # feel free to change this link on your end
     apiURL = 'http://localhost:8080';
 } else {
-    //Vercel link will be placed here after deploying the site
-    apiURL = "PRODUCTION_LINK_GOES_HERE";
+    //Railway link will be placed here after deploying the site
+    apiURL = "capstone-group-3-production.up.railway.app";
 }
 
 //Example Use: axios.get(`${baseURL}/item/`) etc
